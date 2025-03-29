480d014f7a627d617cf429ad76e5018f4c2538f8
Гончаров Даниил <dgoncharov@rnds.pro>
2025-03-29 11:58:07 +0300

    Add landing page

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -5,4 +5,10 @@ class PublicController < ApplicationController
   def tours
     @tours = Tour.where(hidden: false).order(created_at: :desc)
   end
+
+  def tour
+    @tour = Tour.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    redirect_to tour_path
+  end
 end
